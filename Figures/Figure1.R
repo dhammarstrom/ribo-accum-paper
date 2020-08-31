0c7c5c750f5bb1499a180a74662edcdc50c4d0dc
Daniel Hammarström <d.hammarstrom@hotmail.com>
2020-08-31 20:05:46 +0200

    msic updates to modeling selections

--- Figures/Figure1.R
+++ Figures/Figure1.R
@@ -591,8 +591,90 @@ comp_panel <- em %>%
 strength_results <- plot_grid(raw_scores, comp_panel, nrow = 2, align = "v")
 
 
+# Bayesian alternative BRMS ########################
 
- 
+# brms will not ignore rank deficient columns from the design matrix. Factors needs 
+# to be re-formulated in combination from the data frame. This will solve similar problems 
+# in other implementations (e.g. nlme::lme). Post-hoc test can be used to check interactions.
+
+
+strength_long3 <- strength_long %>%
+  # Time coefficients are set as baseline/post
+  mutate(time_pp = if_else(time == "post1w", "post", time),
+         # The de-training period get its own coefficient
+         detrain = if_else(time == "post1w" & group == "int", "detrain", "train"),
+         # The effect of de training will be added to the model --within-- the intervention group 
+         detrain = factor(detrain, levels = c("train", "detrain")), 
+         tx = paste(time_pp, group, detrain, sep = "_"), 
+         tx = factor(tx, levels = c("baseline_con_train", 
+                                    "post_con_train", 
+                                    "baseline_int_train", 
+                                    "post_int_train", 
+                                    "post_int_detrain"))) %>%
+  # Removes duplicated data used for paralell modeling
+  filter(!(time == "post1w" & group == "con")) %>%
+  print()
+
+
+
+mx <- lmer(torque ~  tx + (1|participant), 
+           data = strength_long3[strength_long3$type == "isok", ])
+
+summary(mx)
+
+
+m1 <- brm(torque ~  tx + (1|participant),
+          
+          warmup = 1000, # number of samples before sampling
+          iter = 3000,  # number of mcmc iterations 
+          cores = 2, # number of cores used in sampling
+          chains = 2, # number of chains
+          seed = 123, # seed for reproducible results
+           
+           
+           
+           data = strength_long3[strength_long3$type == "isok", ])
+
+summary(m1)
+
+
+# "post-hoc" tests would be needed to test interactions. This can be done using 
+# the hypothesis function. Hypotheses are written as character vectors 
+# to create a credible interval around the parameter/estimate of interest -- these can be used for 
+# inference. 
+
+#### This is where IM at ########################
+
+h <- hypothesis(m1, "txpost_int_train - txbaseline_int_train  = txpost_con_train")
+?hypothesis
+h
+
+
+# Model diagnostics
+
+# the ggs function transforms the brms output into a longformat tibble, 
+# that we can use to make different types of plots.
+
+m1_long <- ggs(m1)
+
+
+ggplot(filter(m1_long, Parameter %in% c("b_Intercept",
+                                        "b_txpost_con_train",
+                                        "b_txbaseline_int_train",
+                                        "b_txpost_int_train",
+                                        "b_txpost_int_detrain")),
+       aes(x   = Iteration,
+           y   = value, 
+           col = as.factor(Chain)))+
+  geom_line() +
+  geom_vline(xintercept = 1000)+
+  facet_grid(Parameter ~ . ,
+             scale  = 'free_y',
+             switch = 'y')+
+  labs(title = "Caterpillar Plots", 
+       col   = "Chains")
+
+
 
 # Panel X: Ultra sound -- muscle thickness ########################
 
